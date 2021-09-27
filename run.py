59f8d1e0f5a91ea95509ede806e492b25c50200a
conan-sum <conansum24@gmail.com>
2021-09-27 16:09:27 -0400

    Created docker and heroku file

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+import sys
 from api import app
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    port = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
+    app.run(host='0.0.0.0', port=port)
