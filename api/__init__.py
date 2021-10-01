951efbfcf74847c47d36218a09b8f535e642e712
conan-sum <conansum24@gmail.com>
2021-10-01 16:27:03 -0400

    Deploying with database lecture activity

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,13 @@
 import os
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+
 url = os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ \
         else 'postgresql://postgres:password@localhost/postgres'
+if url.startswith("postgres://"):
+    uri = url.replace("postgres://", "postgresql://", 1)
 server = Flask(__name__)
 server.secret_key = '415d9fdfcc2175c2e8bd7bdb2729befc1e788b88799c46d41d2c1ac4fb11b7ec'
 server.config['SQLALCHEMY_DATABASE_URI'] = url
