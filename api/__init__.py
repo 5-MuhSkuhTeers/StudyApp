e75f2616b25c7eb823bbec615fb0948467f6a651
conan-sum <conansum24@gmail.com>
2021-10-01 16:30:53 -0400

    Deploying with database lecture activity

--- api/__init__.py
+++ api/__init__.py
@@ -7,7 +7,7 @@ from flask_bcrypt import Bcrypt
 url = os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ \
         else 'postgresql://postgres:password@localhost/postgres'
 if url.startswith("postgres://"):
-    uri = url.replace("postgres://", "postgresql://", 1)
+    url = url.replace("postgres://", "postgresql://", 1)
 server = Flask(__name__)
 server.secret_key = '415d9fdfcc2175c2e8bd7bdb2729befc1e788b88799c46d41d2c1ac4fb11b7ec'
 server.config['SQLALCHEMY_DATABASE_URI'] = url
