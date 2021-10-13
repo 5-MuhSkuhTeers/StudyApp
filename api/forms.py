64f997e0c4918c2a62df7ffdd940a17ba7ea4e45
alejandraLuGa <47649268+alejandraLuGa@users.noreply.github.com>
2021-10-13 00:27:53 -0400

    Added and edited files to include forget password forms and links to pages. #38

--- api/forms.py
+++ api/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(), Email()])
     password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class RequestForgotPasswordForm (FlaskForm):
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ForgotPasswordForm (FlaskForm):
+    password = PasswordField(label='New Password', validators=[DataRequired()])
+    confirm = PasswordField('Confirm New Password', validators=[
+        DataRequired(),
+        EqualTo('password',message='Passwords Must Match')
+    ])
+    submit = SubmitField('Login')
+
--- api/routes.py
+++ api/routes.py
@@ -1,5 +1,5 @@
 from api import app
-from api.forms import LoginForm
+from api.forms import LoginForm, RequestForgotPasswordForm, ForgotPasswordForm
 from flask import render_template, flash
 
 @app.route("/", methods=['GET','POST'])
@@ -8,3 +8,18 @@ def home():
     if form.validate_on_submit():
         flash("login successful",'success')
     return render_template("login.html",form=form)
+
+#needs to change in order to be able to send the email 
+@app.route("/request-reset-password", methods=['GET','POST'])
+def requestforgotPassword():
+    form = RequestForgotPasswordForm()
+    if form.validate_on_submit():
+        flash("login successful",'success')
+    return render_template("requestResetPassword.html",form=form)
+#also this
+@app.route("/reset-password", methods=['GET','POST'])
+def forgotPassword():
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        flash("login successful",'success')
+    return render_template("resetPassword.html",form=form)
