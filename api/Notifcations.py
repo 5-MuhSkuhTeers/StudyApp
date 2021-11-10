b7c71bbbfd53a61e9af0f522404f655c2c8a9782
zemingzhang1 <60407870+zemingzhang1@users.noreply.github.com>
2021-11-10 14:25:27 -0500

    User Story#8 =>>remind assignments due.

--- api/Notifcations.py
+++ api/Notifcations.py
@@ -44,38 +44,51 @@ class AssigmentNotifcations:
       hrs, mins = splits[1].split(":")[0], splits[1].split(":")[1]
       return int(days)*24*60 + int(hrs)*60 + int(mins)
 
-    # if task due in many days- send email 24 hr before due; if task due in 24- send email 1 hr before due, if task due in 30 mins - send email right now before due
+    # if task due in many days- send email 24 hr before due; if task due in 24- send email 1 hr before due, if task due in 60 mins - send email right now before due
     def setNotifications(self):
         # this is injected text
         inject = " "
+
         # grab todays's date
         today = datetime.datetime.today().replace(second= 0, microsecond=0)
+
         # parse the date and times 
         dueDate = self.parseDate()
         dueTime = self.parseTime()
         dueDateTime = datetime.datetime.combine(dueDate, dueTime)
 
-        #check to see if it is due within 30 mins
-        if self.convertTimedeltaToMin(dueDateTime.replace(second= 0, microsecond=0) - today) < (60/2):
+        # duration is the dueDateTime - todayDateTime
+        duration = self.convertTimedeltaToMin(dueDateTime.replace(second= 0, microsecond=0) - today)
+
+        #check to see if it is due within 60 mins
+        if duration < (60):
           inject = today.strftime("%a, %Y-%b-%d %H:%M:%S -0000")
-        #check to see if it is due within the a day (24 hours) but not within 30 mins
-        elif self.convertTimedeltaToMin(dueDateTime.replace(second= 0, microsecond=0) - today) < (60*24):
+        #check to see if it is due within the a day (24 hours) but not within 60 mins
+        elif duration < (60*24):
           inject = (dueDateTime - timedelta(hours=1)).strftime("%a, %Y-%b-%d %H:%M:%S -0000")
-        #check if it is not due with in (24 hours) and in 30 mins- send email 24 hours before due 
+        #check if it is not due with in (24 hours) and in 60 mins- send email 24 hours before due 
         else:
           inject = (dueDateTime - timedelta(hours=24)).strftime("%a, %Y-%b-%d %H:%M:%S -0000")
 
         # this is to make the post request to the mailgun api
         postHTTP = f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages"
         auth=("api", MAILGUN_API_KEY)
-        # the api mail "o:deliverytime" must be in this format ==>> "o:deliverytime": "Fri, 25 May 2020 23:10:10 -0000"
-        data = {
-          "from": f"Study App <mailgun@{MAILGUN_DOMAIN}>",
-          "to": [self.email],
-          "subject": f"<<Assigment>> {self.taskName}",
-          "text": f"You are receiving this because a request has been made to reimnd you that an assigment ({self.taskName}) is due soon-- on {self.dueDate} at {self.dueTime}-- please check your planner to vertify!",
-          "o:deliverytime": f"{inject}"
-        }
-        return requests.post(postHTTP, auth=auth, data = data)
 
+        # if due in less than an hour send the email right now !!! 
+        if duration < 60:
+          data = {
+            "from": f"Study App <mailgun@{MAILGUN_DOMAIN}>",
+            "to": [self.email],
+            "subject": f"<<Assigment>> {self.taskName}",
+            "text": f"You are receiving this because a request has been made to reimnd you that an assigment ({self.taskName}) is due soon-- on {self.dueDate} at {self.dueTime}-- please check your planner to vertify!"
+          }
+        else: 
+          data = {
+            "from": f"Study App <mailgun@{MAILGUN_DOMAIN}>",
+            "to": [self.email],
+            "subject": f"<<Assigment>> {self.taskName}",
+            "text": f"You are receiving this because a request has been made to reimnd you that an assigment ({self.taskName}) is due soon-- on {self.dueDate} at {self.dueTime}-- please check your planner to vertify!",
+            "o:deliverytime": f"{inject}"
+          }
+        return requests.post(postHTTP, auth=auth, data = data)
 
